394a2739923f9fa809cd0dd068b9c091d7a5cad4
Andy-Gibbons <Tecnfin97@outlook.com>
2025-04-08 21:55:07 +0000

    App.css

---
+++ octofit-tracker/frontend/src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import favicon from './favicon.ico';
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
